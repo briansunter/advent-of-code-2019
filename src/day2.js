ae82e7558f303475cd8dd84e4f3aab1956cbda9c
Brian Sunter <brian@briansunter.com>
2019-12-02 23:30:19 -0800

    Day 2

---
+++ src/day2.js
@@ -0,0 +1,49 @@
+// https://adventofcode.com/2019/day/2
+
+function runOpCode(chunk, program) {
+  const [opCode, num1Pos, num2Pos, resultPos] = chunk;
+  const num1 = program[num1Pos];
+  const num2 = program[num2Pos];
+
+  if (opCode === 1) {
+    program[resultPos] = (num1 + num2);
+  } else if (opCode === 2) {
+    program[resultPos] = (num1 * num2);
+  }
+}
+
+function runProgram(program) {
+  const myProgram = program;
+  for (let i = 0; i < myProgram.length && myProgram[i] !== 99; i = i + 4) {
+    runOpCode(myProgram.slice(i, i+4), myProgram);
+  }
+}
+
+function restoreGravityAssist(program) {
+  setNounVerb(program, 12, 2);
+}
+
+function setNounVerb(program, noun, verb) {
+  program[1] = noun;
+  program[2] = verb;
+}
+
+function findNounVerb(program) {
+  for (let i = 0; i< 99; i++){
+    for (let j = 0; j< 99; j++){
+      const newProgram = [...program];
+      setNounVerb(program, i, j);
+
+      runProgram(newProgram);
+      const result = newProgram[0];
+      if (result === 19690720) {
+        return 100 * i + j;
+      }
+    }
+  }
+  throw `result not found`;
+}
+
+module.exports = {part1: runProgram,
+                  part2: findNounVerb,
+                  restoreGravityAssist };
---
+++ src/day2.test.js
@@ -0,0 +1,47 @@
+const {part1, part2, restoreGravityAssist }= require('./day2');
+const fs  = require('fs');
+
+describe('part 1', () => {
+  test('example 1', () => {
+    const input = [1,0,0,0,99];
+    part1(input);
+    expect(input).toStrictEqual([2,0,0,0,99]);
+  });
+
+  test('example 2', () => {
+    const input = [2,3,0,3,99];
+    part1(input);
+    expect(input).toStrictEqual([2,3,0,6,99]);
+  });
+
+  test('example 3', () => {
+    const input = [1,1,1,4,99,5,6,0,99];
+    part1(input);
+    expect(input).toStrictEqual([30,1,1,4,2,5,6,0,99]);
+  });
+
+  test('example input', () => {
+    const input = fs.readFileSync('src/day2.txt')
+          .toString()
+          .trim()
+          .split(',')
+          .map(x => parseInt(x, 10));
+
+    restoreGravityAssist(input);
+    part1(input);
+
+    expect(input[0]).toBe(3306701);
+  });
+});
+
+describe('part 2', () => {
+  test('example input', () => {
+    const input = fs.readFileSync('src/day2.txt')
+          .toString()
+          .trim()
+          .split(',')
+          .map(x => parseInt(x, 10));
+
+    expect(part2(input)).toBe(7621);
+  });
+});
